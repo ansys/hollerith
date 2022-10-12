065cf4fcd633f26c2981a82ef3de635f2351a3b3
Mohamed Koubaa <mohamed.koubaa@ansys.com>
2022-10-12 10:32:38 -0500

    formatting

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,6 +1,7 @@
 import hollerith as holler
 import pytest
 
+
 def test_no_write():
     with pytest.raises(AttributeError):
         holler.write_int(11, 11, 11)
@@ -8,7 +9,8 @@ def test_no_write():
 
 def test_write_not_callable():
     class K:
-        write=1
+        write = 1
+
     with pytest.raises(SystemError):
         holler.write_int(K(), 11, 11)
 
@@ -17,5 +19,6 @@ def test_write_callable_incorrect_args():
     class K:
         def write(self):
             pass
+
     with pytest.raises(SystemError):
         holler.write_int(K(), 11, 11)
