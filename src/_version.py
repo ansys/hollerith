035c55b3cea2e3f9b8240b7ceb303ed4b20ef5e5
Mohamed Koubaa <koubaa@github.com>
2024-07-11 07:38:23 -0500

    bump version 4

--- src/_version.py
+++ src/_version.py
@@ -7,7 +7,7 @@ version_info = 0, 4, 'dev0'
 
 """
 # major, minor, patch
-version_info = 0, 4, "dev0"
+version_info = 0, 4, "0"
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
