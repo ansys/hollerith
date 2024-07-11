4d32ffa90f6f6c3d74c53e1132cf1482bbca11ba
Mohamed Koubaa <koubaa@github.com>
2024-07-11 07:39:21 -0500

    change to 0.5 dev

--- src/_version.py
+++ src/_version.py
@@ -1,13 +1,13 @@
 """Version of hollerith module.
 
-On the ``master`` branch, use 'dev0' to denote a development version.
+On the ``main`` branch, use 'dev0' to denote a development version.
 For example:
 
-version_info = 0, 4, 'dev0'
+version_info = 0, 5, 'dev0'
 
 """
 # major, minor, patch
-version_info = 0, 4, "dev0"
+version_info = 0, 5, "dev0"
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
