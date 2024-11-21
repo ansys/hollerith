0e948cbc0e511667590c1fe460dfb40672153dc3
Mohamed Koubaa <koubaa@github.com>
2024-11-21 09:06:38 -0600

    drop 3.9

--- setup.py
+++ setup.py
@@ -44,8 +44,6 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX",
         "Operating System :: MacOS",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
