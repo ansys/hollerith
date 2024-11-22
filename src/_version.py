e71cce7b270f419b2ff679259052aec082de68bc
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-11-22 09:49:59 +0100

    relese: v0.5.1

--- src/_version.py
+++ src/_version.py
@@ -3,11 +3,11 @@
 On the ``main`` branch, use 'dev0' to denote a development version.
 For example:
 
-version_info = 0, 5, 0
+version_info = 0, 5, 1
 
 """
 # major, minor, patch
-version_info = 0, 5, 0
+version_info = 0, 5, 1
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
