849b0419e4aa7346fad1e5086279cf04f1e272d3
Mohamed Koubaa <koubaa@github.com>
2025-01-03 08:00:02 -0600

    bump dev version to 0.7.dev0

--- src/_version.py
+++ src/_version.py
@@ -3,11 +3,11 @@
 On the ``main`` branch, use 'dev0' to denote a development version.
 For example:
 
-version_info = 0, 6, 'dev0'
+version_info = 0, 7, 'dev0'
 
 """
 # major, minor, patch
-version_info = 0, 6, "dev0"
+version_info = 0, 7, "dev0"
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
